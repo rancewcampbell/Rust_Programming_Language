a0a3780c2471d42cd2b2f62abc7155498eeaf208
Rance Campbell <rance@oulawcoding.com>
2021-03-06 14:19:18 -0700

    finish ch8 ex2 - pig_latinify

--- ch_08_ex/src/main.rs
+++ ch_08_ex/src/main.rs
@@ -3,10 +3,6 @@
 // sorted, the value in the middle position), and mode (the value that occurs most often; a hash
 // map will be helpful here) of the list.
 //
-// [] Convert strings to pig latin. The first consonant of each word is moved to the end of the word
-// and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added
-// to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
-//
 // [] Using a hash map and vectors, create a text interface to allow a user to add employee names to
 // a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then
 // let the user retrieve a list of all people in a department or all people in the company by
@@ -16,6 +12,7 @@
 
 use std::collections::HashMap;
 
+// Exercise 1 functions:
 fn find_mean(int_list: &Vec<i32>) -> f64 {
     let int_list_sum: i32 = int_list.iter().sum();
     int_list_sum as f64 / int_list.len() as f64
@@ -33,30 +30,59 @@ fn find_median(int_list: &Vec<i32>) -> f64 {
     }
 }
 
-fn find_mode(int_list: &Vec<i32>) -> &i32 {
+fn find_mode(int_list: &Vec<i32>) -> i32 {
     let mut mode_map = HashMap::new();
 
     for num in int_list {
         *mode_map.entry(num).or_insert(0) += 1;
     }
 
-    mode_map
+    *mode_map
         .into_iter()
         .max_by_key(|&(_, v)| v)
         .map(|(v, _)| v)
         .unwrap()
 }
 
-fn ex1() {
-    let int_list = vec![1, 3, 2];
+// [x] Convert strings to pig latin. The first consonant of each word is moved to the end of the word
+// and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added
+// to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+//
+// Exercise 2 functions:
+fn pig_latinify(s: &String) -> String {
+    let phrase = s.clone().to_lowercase();
+    let mut pl_phrase: Vec<String> = Vec::new();
+    let iter = phrase.split_whitespace();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
 
-    let mean = find_mean(&int_list);
-    let median = find_median(&int_list);
-    let mode = find_mode(&int_list);
-
-    println!("mean: {}, median: {}, mode {:?}", mean, median, *mode);
+    for i in iter {
+        let mut word = i.chars();
+        let first_char = word.next().unwrap();
+        let mut pl_word = String::new();
+        if vowels.contains(&first_char) {
+            pl_word.push_str(format!("{}-hay", i).as_str());
+            pl_phrase.push(pl_word);
+        } else {
+            pl_word.push_str(format!("{}-{}ay", word.as_str(), first_char).as_str());
+            pl_phrase.push(pl_word);
+        }
+    }
+    pl_phrase.join(" ")
 }
 
 fn main() {
-    ex1();
+    let int_list = vec![1, 3, 5, 4, 7, 3, 6, 4, 21, 65, 3];
+    let int_list_mean = find_mean(&int_list);
+    let int_list_median = find_median(&int_list);
+    let int_list_mode = find_mode(&int_list);
+
+    println!(
+        "mean: {}, median: {}, mode {}",
+        int_list_mean, int_list_median, int_list_mode
+    );
+
+    let s = String::from("What is going on my peeps");
+    let pl_s = pig_latinify(&s);
+
+    println!("{}", pl_s);
 }
