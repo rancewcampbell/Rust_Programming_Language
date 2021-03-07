68f4a27df9ebbaeff461e5aa148f9f2f61b77e1d
Rance Campbell <rance@oulawcoding.com>
2021-03-06 20:40:09 -0700

    finish ch8 ex3

--- ch_08_ex/src/main.rs
+++ ch_08_ex/src/main.rs
@@ -3,10 +3,6 @@
 // sorted, the value in the middle position), and mode (the value that occurs most often; a hash
 // map will be helpful here) of the list.
 //
-// [] Using a hash map and vectors, create a text interface to allow a user to add employee names to
-// a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then
-// let the user retrieve a list of all people in a department or all people in the company by
-// department, sorted alphabetically.
 //
 //
 
@@ -47,8 +43,9 @@ fn find_mode(int_list: &Vec<i32>) -> i32 {
 // [x] Convert strings to pig latin. The first consonant of each word is moved to the end of the word
 // and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added
 // to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
+// TODO: deal with capitalzing first char in string & parse punctuation
 //
-// Exercise 2 functions:
+// Exercise 2 function:
 fn pig_latinify(s: &String) -> String {
     let phrase = s.clone().to_lowercase();
     let mut pl_phrase: Vec<String> = Vec::new();
@@ -70,6 +67,74 @@ fn pig_latinify(s: &String) -> String {
     pl_phrase.join(" ")
 }
 
+// [x] Using a hash map and vectors, create a text interface to allow a user to add employee names to
+// a department in a company. For example, “Add Sally to Engineering” or “Add Amir to Sales.” Then
+// let the user retrieve a list of all people in a department or all people in the company by
+// department, sorted alphabetically.
+
+fn company_database_cli() {
+    use std::io;
+
+    let mut input = String::new();
+    let mut get_input = true;
+    let mut instructions: Vec<String> = Vec::new();
+
+    let mut departments: HashMap<String, Vec<String>> = HashMap::new();
+
+    println!("Enter an instruction (enter to quit)");
+
+    while get_input {
+        match io::stdin().read_line(&mut input) {
+            Ok(_) => {
+                get_input = !input.trim().is_empty();
+                if get_input {
+                    instructions.push(String::from(input.trim()));
+                }
+                input.clear();
+            }
+            Err(e) => {
+                println!("What did you type?? {}", e);
+                break;
+            }
+        }
+    }
+
+    for i in instructions.iter() {
+        let mut instruction = i.split_whitespace();
+        let cmd = instruction.next().unwrap();
+        let name = instruction.next().unwrap();
+        let dept = instruction.next().unwrap();
+
+        match cmd {
+            "add" => {
+                departments
+                    .entry(String::from(dept))
+                    .or_insert(Vec::new())
+                    .push(String::from(name));
+            }
+            "rmv" => {
+                let mut employees = departments.get(&dept.to_string()).unwrap().clone();
+                if let Some(pos) = employees.iter().position(|s| s == name) {
+                    employees.remove(pos);
+                }
+                departments.insert(String::from(dept), employees);
+            }
+            &_ => {
+                println!("That's not a command");
+            }
+        }
+    }
+
+    for (k, v) in &mut departments {
+        println!("{}:", k);
+        v.sort();
+        for i in v {
+            println!("\t{}", i);
+        }
+        println!();
+    }
+}
+
 fn main() {
     let int_list = vec![1, 3, 5, 4, 7, 3, 6, 4, 21, 65, 3];
     let int_list_mean = find_mean(&int_list);
@@ -85,4 +150,6 @@ fn main() {
     let pl_s = pig_latinify(&s);
 
     println!("{}", pl_s);
+
+    company_database_cli();
 }
